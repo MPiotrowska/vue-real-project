c5491e812bc1bd8b6808592ff37749c3f242f47c
Monika Piotrowska <monikapiotrowska27@gmail.com>
2022-01-22 21:35:45 +0100

    addnew component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,15 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/EventCard.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     HelloWorld,
   },
-};
+}
 </script>
+
+
+
