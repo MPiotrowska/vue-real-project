d1c367433fc66fcbcb8e1e6b9a8070647e5fa64f
Monika Piotrowska <monikapiotrowska27@gmail.com>
2022-01-23 12:53:33 +0100

    create Event service

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,7 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
-import axios from 'axios'
+import EventService from '@/services/EventService.js'
 
 export default {
   name: 'EventList',
@@ -21,8 +21,7 @@ export default {
     }
   },
   created() {
-    axios
-      .get('https://my-json-server.typicode.com/MPiotrowska/my-server/events')
+    EventService.getEvents()
       .then((response) => {
         this.events = response.data
         // console.log('eve', response.data)
