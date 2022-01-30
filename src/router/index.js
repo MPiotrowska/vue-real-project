bd206e4ea8179164767cab98504d56b643637aa9
Monika Piotrowska <monikapiotrowska27@gmail.com>
2022-01-30 16:02:15 +0100

    add nested routes and redirections

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,57 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import EventDetails from "../views/EventDetails.vue";
-import About from "../views/About.vue";
-
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventLayout from '../views/event/Layout.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "EventList",
+    path: '/',
+    name: 'EventList',
     component: EventList,
     props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
-    path: "/about",
-    name: "About",
-    component: About
+    path: '/events/:id',
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
   },
   {
-    path: "/event/:id",
-    name: "EventDetails",
-    props: true,
-    component: EventDetails
+    path: '/event/:afterEvent(.*)',
+    redirect: to => {
+      return { path: '/events/' + to.params.afterEvent }
+    }
   },
-];
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
